953d19b5ef7730704e90d60ffdbccf3f250cb8d0
khuynhthanh <phamanhtuan1406@gmail.com>
2019-08-09 17:56:23 +0700

    update-avatar: cập nhật update-info - file avatar

--- src/app/model/request/userManager/update-info.ts
+++ src/app/model/request/userManager/update-info.ts
@@ -2,10 +2,12 @@ export class UpdateInfo {
   firstName: string;
   lastName: string;
   email: string;
+  avatar: File;
 
-  constructor(firstName: string, lastName: string, email: string) {
+  constructor(firstName: string, lastName: string, email: string, avatar: File) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
+    this.avatar = avatar;
   }
 }
