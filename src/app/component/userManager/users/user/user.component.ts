0836bc975829b49dea302a0455a7d60487ebe5cc
khuynhthanh <phamanhtuan1406@gmail.com>
2019-08-09 17:59:47 +0700

    update-avatar: update use component

--- src/app/component/userManager/users/user/user.component.ts
+++ src/app/component/userManager/users/user/user.component.ts
@@ -2,20 +2,44 @@ import {Component, OnInit} from '@angular/core';
 import {UserService} from '../../../../services/userManager/user/user.service';
 import {TokenStorageService} from '../../../../services/userManager/token/token-storage.service';
 
+function AuthInfo() {
+  if (this.token.getToken()) {
+    if (this.token.getAvatarLink() !== '') {
+      this.avatarLink = this.token.getAvatarLink();
+    }
+    this.roles = this.token.getAuthorities();
+    this.roles.every(role => {
+      if (role === 'ROLE_ADMIN') {
+        this.authority = 'admin';
+        return false;
+      } else if (role === 'ROLE_PM') {
+        this.authority = 'pm';
+        return false;
+      }
+      this.authority = 'user';
+      return true;
+    });
+  }
+}
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+  private avatarLink: string;
   board: string;
   errorMessage: string;
   info: any;
+  private roles: string[];
+  protected authority: string;
 
   constructor(private userService: UserService, private token: TokenStorageService) {
   }
 
   ngOnInit() {
+    AuthInfo.call(this);
     this.userService.getUserBoard().subscribe(
       data => {this.board = data; },
       error => {this.errorMessage = `${error.status}: ${JSON.parse(error.error).message}`; });
